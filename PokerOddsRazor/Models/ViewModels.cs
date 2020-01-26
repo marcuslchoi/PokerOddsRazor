e5c77f3d9b45a1465a53bb5e50c427af30f187e5
Marcus Choi <marcuslchoi@gmail.com>
2020-01-25 21:59:34 -0800

    Added ProbabilityViewModel
    Signed-off-by: Marcus Choi <marcuslchoi@gmail.com>

---
+++ PokerOddsRazor/Models/ViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace PokerOddsRazor.Models
+{
+    public class ProbabilityViewModel
+    {
+        public double HighCard { get; set; }
+        public double Pair { get; set; }
+        public double TwoPair { get; set; }
+        public double ThreeOfAKind { get; set; }
+        public double Straight { get; set; }
+        public double Flush { get; set; }
+        public double FullHouse { get; set; }
+        public double FourOfAKind { get; set; }
+        public double StraightFlush { get; set; }
+        public double RoyalFlush { get; set; }
+
+        public ProbabilityViewModel()
+        {
+        }
+    }
+}
