a496cab78e8d6b483ff55eeb3fd9834d1a2ac3d6
Marcus Choi <marcuslchoi@gmail.com>
2020-02-04 11:36:59 -0800

    hide previously used card buttons on submit
    Signed-off-by: Marcus Choi <marcuslchoi@gmail.com>

--- PokerOddsRazor/Pages/Holdem.cshtml.cs
+++ PokerOddsRazor/Pages/Holdem.cshtml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PokerOddsRazor.Models;
+using Newtonsoft.Json;
 
 namespace PokerOddsRazor.Pages
 {
